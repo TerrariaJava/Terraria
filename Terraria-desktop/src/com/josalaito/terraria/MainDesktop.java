60f2e0531ad184a3a74f831bf13a5c0be4dda372
TerrariaJava <terrariajava@gmail.com>
2014-05-01 17:41:07 +0200

    Merged with eclipse and fixed Desktop project

--- Terraria-desktop/src/com/josalaito/terraria/MainDesktop.java
+++ Terraria-desktop/src/com/josalaito/terraria/MainDesktop.java
@@ -3,7 +3,7 @@ package com.josalaito.terraria;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
+public class MainDesktop {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Terraria";
